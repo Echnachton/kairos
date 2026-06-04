63eb3e762c26566ef9f204f8813b96438a4af440
Echnachton <50080349+Echnachton@users.noreply.github.com>
2026-06-04 10:09:04 +0200

    basic hono setup

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+import { serveStatic } from "hono/bun";
+import { join } from "node:path";
+
+const WEB_ROOT = join(import.meta.dirname, "web");
+
+const app = new Hono();
+
+app.use('*', serveStatic({ 
+    root: WEB_ROOT
+}));
+
+app.onError((err, c) => {
+    if (err instanceof HTTPException) {
+        return err.getResponse();
+    }
+
+    console.error(err);
+    return c.text("Internal Server Error", 500);
+});
+
+app.get("/api", (c) => c.text("Hello World"));
+
+export default app;
